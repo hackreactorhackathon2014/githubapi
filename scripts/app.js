ec81307ceecd6130bcf3ed2db800b37e014137ee
John Tan <tantai.john@gmail.com>
2014-11-20 21:34:53 -0800

    Updated nvd3.

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,11 @@
 $(function(){
   nv.addGraph(function() {
   var chart = nv.models.scatterChart()
-                .showDistX(true)    //showDist, when true, will display those little distribution lines on the axis.
+                .showDistX(true)   
                 .showDistY(true)
                 .transitionDuration(350)
                 .color(d3.scale.category10().range());
 
-  //Configure how the tooltip looks.
   chart.tooltipContent(function(key) {
       return '<h3>' + key + '</h3>';
   });
@@ -14,7 +13,6 @@ $(function(){
   //Axis settings
   chart.xAxis.axisLabel('PRICE RANGE').tickFormat(d3.format('.02f'));
   chart.yAxis.axisLabel('STARS').tickFormat(d3.format('.02f'));
-  //We want to show shapes other than circles.
   chart.scatter.onlyCircles(false);
 
   var myData = randomData(5,40);
@@ -28,13 +26,10 @@ $(function(){
   return chart;
 });
 
-/************************************** 
- * Simple test data generator
- */
-function randomData(groups, points) { //# groups,# points per group
+function randomData(groups, points) { 
   var data = [],
       shapes = ['circle', 'cross', 'triangle-up', 'diamond', 'square'],
-      random = d3.random.normal()
+      random = d3.random.normal(5)
       groupsList = ['American', 'Italian', 'Japanese', 'Chinese', 'Mexican'];
   for (i = 0; i < groups; i++) {
     data.push({
@@ -46,8 +41,8 @@ function randomData(groups, points) { //# groups,# points per group
       data[i].values.push({
         x: random()
       , y: random()
-      , size: Math.random()   //Configure the size of each scatter point
-      , shape: (Math.random() > 0.95) ? shapes[j % 6] : "circle"  //Configure the shape of each scatter point.
+      , size: Math.random()   
+      , shape: (Math.random() > 0.95) ? shapes[j % 6] : "circle" 
       });
     }
   }
